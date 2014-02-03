40471ae6c4b6ca150bbcbd6d195b39c47a09ad90
Shaun <discospider@me.com>
2014-02-03 15:12:45 -0500

    forgot one variable

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 class htop::install {
   if !defined(Package[$htop::params::package_name]) {
-    package { $ssh::params::package_name:
+    package { $htop::params::package_name:
       ensure => present,
     }
   }
